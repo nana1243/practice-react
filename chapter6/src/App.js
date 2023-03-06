a4a9a723ce9c08d1756986beeb66b5bc73757796
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2023-03-06 22:54:09 +0900

    [Chap6] 컴포넌트의 반복(#3)

---
+++ chapter6/src/App.js
@@ -0,0 +1,13 @@
+import IterationSample from "./components/IterationSample";
+import IterationSampleV2 from "./components/IterationSampleV2";
+
+function App() {
+  return (
+    <div className="App">
+      {/*<IterationSample/>*/}
+      <IterationSampleV2/>
+    </div>
+  );
+}
+
+export default App;
