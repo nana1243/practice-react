26ad1b6cbd7013328c0f74f38d754f623ed5d97e
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2023-03-12 13:32:41 +0900

    fea) 최적화 작업

--- chapter10/src/AppV2.js
+++ chapter10/src/AppV2.js
@@ -1,7 +1,7 @@
 import TodoTemplate from "./components/TodoTemplate/TodoTemplate";
 import TodoInsert from "./components/TodoInsert/TodoInsert";
 import TodoList from "./components/TodoList/TodoList";
-import {useCallback, useReducer, useRef, useState} from "react";
+import {useCallback, useRef, useState} from "react";
 
 function createBulkTodos() {
     const array = [];
@@ -15,39 +15,30 @@ function createBulkTodos() {
     return array;
 }
 
-function todoReducer(todos, action){
-    switch (action.type){
-        case 'INSERT':
-            return todos.concat(action.todo)
-
-        case 'REMOVE':
-            return todos.filter(todo => todo.id !== action.id)
-
-        case 'TOGGLE':
-            return todos.map(todo =>  todo.id === action.id ? {...todo, checked: !todo.checked} : todo)
-    }
-}
-
 function App() {
-    const [todos, dispatch] = useReducer(todoReducer,undefined,createBulkTodos);
-    const nextId = useRef(11);
+    const [todos, setTodos] = useState(createBulkTodos)
+    const nextId = useRef(2501);
     const onInsert = useCallback(text => {
         const todo = {
             id: nextId.current,
             text,
             checked: false
         }
-        dispatch({type : 'INSERT', todo})
+        setTodos(todos => todos.concat(todo));
         nextId.current += 1
 
     }, []);
 
     const onRemove = useCallback(id => {
-        dispatch({type : 'REMOVE' , id})
+        setTodos(todos => todos.filter(todo => todo.id !== id))
     }, [])
 
     const onToggle = useCallback(id => {
-        dispatch({type : 'TOGGLE' , id})
+        setTodos(todos=> todos.map(
+            todo => {
+                return todo.id === id ? {...todo, checked: !todo.checked} : todo
+            }
+        ));
     }, [])
 
     return (
@@ -64,4 +55,4 @@ function App() {
     );
 }
 
-export default App;
+// export default App;
