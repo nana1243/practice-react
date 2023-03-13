8298b8b65852617b1248b19aa2559408fb77165e
HeuiEun Choi <52269210+nana1243@users.noreply.github.com>
2023-03-13 20:46:54 +0900

    [Chap13] 라우터의이해 (#7)
    - 더 보충해야함

---
+++ chapter13/src/App.js
@@ -0,0 +1,23 @@
+import './App.css';
+import {Route, BrowserRouter, Routes} from 'react-router-dom'
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Profile from "./pages/Profile";
+import Articles from "./pages/Articles";
+import Article from "./pages/Article";
+
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/about" element={<About/>}/>
+                <Route path="/profiles/:username" element={<Profile/>}/>
+                <Route path="/articles" element={<Articles/>}></Route>
+                <Route path="/articles/:id" element={<Article/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    );
+}
+
+export default App;
